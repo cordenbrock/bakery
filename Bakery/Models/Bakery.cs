e803f11ea82edcd674933cd2e099c7e7c0db34b3
cordenbrock <cordenbrock@gmail.com>
2020-12-11 12:12:55 -0800

    Add function for adjusting bread cost

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -1,9 +1,44 @@
+using System;
+
 namespace Bakery.Models
 {
+    // Order Class
+    public class Order
+    {
+
+    }
+
+
+
+    // Bread class
     public class Bread
     {
-        // code
+        // Auto-implemented fields
+        public int BreadCost { get; set; }
+
+        // Bread constructor
+        public Bread(int breadCost)
+        {
+            BreadCost = breadCost;
+        }
+
+        // Calculate bread deal adjustment
+        public int AdjustBreadCost(int loaves)
+        {
+            int adjustment = 0;
+            if (loaves >= 3)
+            {
+                double freeLoaves = loaves/3;
+                adjustment = 5 * (int)(Math.Floor(freeLoaves)); 
+            }
+            return adjustment;
+        }
     }
+    
+    
+
+
+    // Pastry class
     public class Pastry
     {
         // code
